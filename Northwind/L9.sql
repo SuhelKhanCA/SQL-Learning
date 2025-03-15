1f3d9de13120bc26c0cdfab1169be50741fd5fbb
suhelkhanca <suhel.hata@gmail.com>
2025-03-16 01:54:43 +0530

    LIMIT and OFFSET statement

--- Northwind/L9.sql
+++ Northwind/L9.sql
@@ -6,3 +6,17 @@ USE northwindmysql;
 SELECT *
 FROM customers
 LIMIT 2; -- Show me 2 record
+
+-- OFFSET : skipping records
+
+SELECT *
+FROM customers
+LIMIT 4 -- record (3 to 6) inclusive
+OFFSET 2; -- skip first two.
+
+-- Q1: second highest
+SELECT *
+FROM products
+ORDER BY UnitPrice DESC
+LIMIT 1 
+OFFSET 1; -- skip first highest.
